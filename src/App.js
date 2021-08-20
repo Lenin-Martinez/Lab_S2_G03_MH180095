bcb3579a99d168d8e1d19a9ca9b5d567e8daf58b
Lenin-Martinez <88004087+Lenin-Martinez@users.noreply.github.com>
2021-08-19 19:44:51 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+  <div className="App">
+    <div className="App-content">
+      <p>
+      Lista de supermercado
+      </p>
+      <Form />
+    </div>
+  </div>
+);
+}
+export default App;
