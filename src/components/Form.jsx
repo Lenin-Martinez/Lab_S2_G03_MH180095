b2200b7c5256b642e76ab7451a8ac6ee7dbe3f82
Lenin-Martinez <88004087+Lenin-Martinez@users.noreply.github.com>
2021-08-20 13:36:38 -0600

    Add files via upload

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,21 +5,30 @@ const Form = () => {
     const [todo, setTodo]=useState({})
     const [todos, setTodos]=useState([])
 
+    const [cantidad, setCantidad]=useState({})
+    const [cantidades, setCantidades]=useState([])
+
 const handleChange = e => setTodo({[e.target.name]: e.target.value})
+const handleChangeC = e => setCantidad({[e.target.name]: e.target.value})
 
 const handleClick = e => {
     if((Object.keys(todo).length === 0 || todo.todo.trim() === '')){
         alert('El campo no puede estar vacio')
         return
     }
-
+    
     setTodos([...todos, todo])
+    setCantidades([...cantidades, cantidad])
 }
 
 const deleteTodo = indice => {
     const newTodos = [...todos]
     newTodos.splice(indice,1)
     setTodos(newTodos)
+    
+    const newCantidades = [...cantidades]
+    newCantidades.splice(indice,1)
+    setCantidades(newCantidades)
 }
 
     return (
@@ -27,11 +36,19 @@ const deleteTodo = indice => {
         <form onSubmit = { e=> e.preventDefault()}>
             <label>Agregar producto</label><br />
             <input type='Text' name='todo' onChange={handleChange} />
+            <input type='Text' name='cantidad' onChange={handleChangeC} />
             <button onClick={handleClick}>agregar</button>
         </form>
+        
         {
-            todos.map((value,index) => (
+            todos.map((value, index) => (
                 <Todo todo={value.todo} key={index} index={index} deleteTodo={deleteTodo}/>
+            ))
+        }
+        
+        {
+            cantidades.map((value, index) => (
+                <Todo todo={value.cantidad + ' unidades'} key={index} index={index} deleteTodo={deleteTodo}/>
             ))
         }
         </>
